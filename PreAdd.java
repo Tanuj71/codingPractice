9c73b1cfcad9915511a03df2cea5284fdcf3e499
Tanuj Gaur <96991464+Tanuj71@users.noreply.github.com>
2023-02-24 17:28:00 +0530

    day 13

---
+++ PreAdd.java
@@ -0,0 +1,10 @@
+public class PreAdd {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int result=0;
+        result=++a*--b+a++;
+        System.out.println(result);
+    }
+}
